91b268f367809521453cec30b2822131bc3cec23
morjina <fmorjina111@gmail.com>
2024-10-12 19:15:41 +0600

    up

---
+++ app.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+  $(".faq1").click(function(){
+      $(".faqp1").toggle(10);
+  });
+});
+$(document).ready(function(){
+  $(".faq2").click(function(){
+      $(".faqp2").toggle(10);
+  });
+});
+$(document).ready(function(){
+  $(".faq3").click(function(){
+      $(".faqp3").toggle(10);
+  });
+});
+$(document).ready(function(){
+  $(".faq4").click(function(){
+      $(".faqp4").toggle(10);
+  });
+});
+$(document).ready(function(){
+  $(".faq5").click(function(){
+      $(".faqp5").toggle(10);
+  });
+});
+$(document).ready(function(){
+  $(".faq6").click(function(){
+      $(".faqp6").toggle(10);
+  });
+});
